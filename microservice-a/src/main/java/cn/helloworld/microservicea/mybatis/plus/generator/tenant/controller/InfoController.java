3c84ce751e8ed3fe9d0d69e4f1e3786c6c177b69
张凯 <425485346@qq.com>
2021-01-28 08:22:06 +0800

    td 数据库调试

---
+++ microservice-a/src/main/java/cn/helloworld/microservicea/mybatis/plus/generator/tenant/controller/InfoController.java
@@ -0,0 +1,20 @@
+package cn.helloworld.microservicea.mybatis.plus.generator.tenant.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 泡泡熊
+ * @since 2021-01-27
+ */
+@RestController
+@RequestMapping("/tenant/info")
+public class InfoController {
+
+}
